b65dcf0b3325f3387e912cf1f2ae8e38a693c81a
Sagar Kale <sagark.kale@hotmail.com>
2020-07-08 21:30:10 +0530

    Update CallmebotApi.java

--- src/main/java/com/sagar/automation/winautomation/api/CallmebotApi.java
+++ src/main/java/com/sagar/automation/winautomation/api/CallmebotApi.java
@@ -5,7 +5,7 @@ import com.sagar.automation.winautomation.enums.NotificationType;
 public final class CallmebotApi {
     private static final String WHATSAPP_API = "https://api.callmebot.com/whatsapp.php";
     private static final String WHATSAPP_API_KEY = "YOUR_WHATSAPP_API_CODE";
-    private static final String TELEGRAM_API = "http://api.callmebot.com/start.php?user=@sgrkale";
+    private static final String TELEGRAM_API = "http://api.callmebot.com/start.php?user=@YOUR_TELEGRAM_USERNAME"; // make sure to authenticate telegram api on browser first and activate
 
     private CallmebotApi() {
     }
@@ -14,8 +14,8 @@ public final class CallmebotApi {
         StringBuilder builder = new StringBuilder();
 
         if (NotificationType.WHATSAPP == type) {
-            builder.append(WHATSAPP_API)
-                    .append("?phone=+919561609535&text=%s&apikey=")
+            builder.append(WHATSAPP_API)1
+                    .append("?phone=YOUR_PHONE_NO_WITH_COUNTRY_CODE&text=%s&apikey=") //phone number with country code e.g +919012345678
                     .append(WHATSAPP_API_KEY);
         } else {
             builder.append(TELEGRAM_API)
